80273a425ab6aa1ab3cd9fb02d9ee5af48100d75
Flavio Zhuka <flavio.zhuka@studio.unibo.it>
2025-03-03 23:22:20 +0100

    login e session_creation aggiunti

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -1,10 +1,17 @@
 "use server";
 
-import { SignupFormSchema, FormState } from "@/app/lib/definitions";
+import {
+  SignupFormSchema,
+  SigninFormSchema,
+  FormState,
+} from "@/app/lib/definitions";
 import bcrypt from "bcrypt";
 import { insertDB } from "../lib/mongodb";
 import { findDB } from "../lib/mongodb";
 import { USERS } from "../lib/mongodb";
+import { generateSessionToken } from "../lib/session";
+import { User } from "../lib/definitions";
+import { cookies } from "next/headers";
 
 export async function signup(state: FormState, formData: FormData) {
   // Validate form fields
@@ -35,12 +42,13 @@ export async function signup(state: FormState, formData: FormData) {
 
   // Payload to compare with existing users in the database
   // Essenzialmente qua diciamo a MongoDB di cercare un documento che abbia lo stesso nome o la stessa email
+  // Diamo per scontato che sia il nome che la email siano unici
   const compare_payload = {
     $or: [{ name: name }, { email: email }],
   };
 
   // Check if user already exists
-  if (findDB(USERS, compare_payload) == null) {
+  if ((await findDB(USERS, compare_payload)) == null) {
     // Insert user into database if they don't already exist
     insertDB(USERS, payload);
     console.log("User inserted into database");
@@ -50,7 +58,54 @@ export async function signup(state: FormState, formData: FormData) {
   }
   // TODO:
 
-  // 4. Create user session
+  // 4. Redirect user to login page
+}
 
-  // 5. Redirect user
+export async function login(state: FormState, formData: FormData) {
+  const validatedFields = SigninFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  // If any form fields are invalid, return early
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  // 2. Prepare data for comparison with database
+  const { email, password } = validatedFields.data;
+
+  // 3. Insert the user into the database
+  // Payload to insert into the database
+  const payload = {
+    email,
+  };
+
+  const user = await findDB<User>(USERS, payload);
+  if (!user) {
+    // If user is not found
+    console.log("User not found");
+    return { error: "User not found" };
+  } else {
+    // If user is found
+    console.log("User found");
+    //Compare the password
+    if (await bcrypt.compare(password, user.password)) {
+      const token = await generateSessionToken(user._id);
+      const biscottino = await cookies();
+      biscottino.set("session", token, {
+        httpOnly: true,
+        secure: true,
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        sameSite: "lax",
+        path: "/",
+      });
+      console.log("Session created");
+    } else {
+      //If the password is incorrect, log a message
+      console.log("Incorrect password");
+    }
+  }
 }
---
+++ src/app/lib/session.ts
@@ -0,0 +1,62 @@
+import "server-only";
+import { SignJWT, jwtVerify } from "jose";
+import { SessionPayload } from "@/app/lib/definitions";
+import { cookies } from "next/headers";
+
+// Prendiamo la chiave segreta dalla variabile d'ambiente e la codifichiamo
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+// Funzione per criptare il payload della sessione
+export async function encrypt(payload: SessionPayload) {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("7d")
+    .sign(encodedKey);
+}
+
+// Funzione per decriptare il payload della sessione
+export async function decrypt(session: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (error) {
+    console.log("Failed to verify session");
+  }
+}
+
+// Funzione per generare il token della sessione
+export async function generateSessionToken(userId: string) {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  return await encrypt({ userId, expiresAt });
+}
+
+// Funzione per aggiornare la sessione
+export async function updateSession() {
+  const session = (await cookies()).get("session")?.value;
+  const payload = await decrypt(session);
+
+  if (!session || !payload) {
+    return null;
+  }
+
+  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  const cookieStore = await cookies();
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    secure: true,
+    expires: expires,
+    sameSite: "lax",
+    path: "/",
+  });
+}
+
+// Funzione per eliminare la sessione
+export async function deleteSession() {
+    const cookieStore = await cookies()
+    cookieStore.delete('session')
+  }
